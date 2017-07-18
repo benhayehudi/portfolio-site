bb937d9bb2a60dce878fe46a6cbebe5047f8d006
benhayehudi <bengreenberg@gmail.com>
2017-07-18 14:09:32 -0400

    added helper methods

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -10,19 +10,19 @@ get '/portfolio/show/:id' do
 end
 
 def blog_link?
-  @item.blog_link == "http://bengreenberg.org/portfolio/show/#{@item.id}"
+  @item.blog_link == "http://bengreenberg.org/portfolio/show/#{@item.id}" || @item.blog_link == "" || @item.blog_link == nil
 end
 
 def website_link?
-  @item.website_link == "http://bengreenberg.org/portfolio/show/#{@item.id}"
+  @item.website_link == "http://bengreenberg.org/portfolio/show/#{@item.id}"|| @item.website_link == "" || @item.website_link == nil
 end
 
 def youtube_link?
-  @item.youtube_link == "http://bengreenberg.org/portfolio/show/#{@item.id}"
+  @item.youtube_link == "http://bengreenberg.org/portfolio/show/#{@item.id}" || @item.youtube_link == "" || @item.youtube_link == nil
 end
 
 def github_link?
-  @item.link == "http://bengreenberg.org/portfolio/show/#{@item.id}"
+  @item.link == "http://bengreenberg.org/portfolio/show/#{@item.id}" || @item.link == "" || @item.link == nil
 end
 
 
