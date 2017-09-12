8b0f8b82c0bb1b8f86bed31f13f620c6a88458db
benhayehudi <bengreenberg@gmail.com>
2017-09-12 09:30:32 -0400

    working on backend API service for new React frontend

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem "tux"
+gem "json"
 
 group :test do
   gem 'rspec'
--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,22 +1,47 @@
 class BlogController < ApplicationController
+  require 'json'
 
+  # Sinatra Frontend
+  get '/blog/all-postings' do
+    @postings = Blog.all
+    erb :'/blog/all-postings'
+  end
 
-get '/blog/all-postings' do
-  @postings = Blog.all
-  erb :'/blog/all-postings'
-end
+  get '/blog/tech-postings' do
+    erb :'/blog/tech-postings'
+  end
 
-get '/blog/tech-postings' do
-  erb :'/blog/tech-postings'
-end
+  get '/blog/jewish-postings' do
+    erb :'/blog/jewish-postings'
+  end
 
-get '/blog/jewish-postings' do
-  erb :'/blog/jewish-postings'
-end
+  get '/blog/show/:id' do
+    @listing = Blog.find_by_id(params[:id])
+    erb :"/blog/show"
+  end
 
-get '/blog/show/:id' do
-  @listing = Blog.find_by_id(params[:id])
-  erb :"/blog/show"
-end
+  # API Service
+  get '/api/blog/index.json' do
+    @postings = Blog.all
+    content_type :json
+    @postings.to_json
+  end
 
+  get '/api/blog/tech.json' do
+    @techposts = Blog.where(publish: [true], tags: ["Tech"])
+    content_type :json 
+    @techposts.to_json
+  end
+
+  get '/api/blog/jewish.json' do
+    @jewishposts = Blog.where(publish: [true], tags: ["Jewish"])
+    content_type :json 
+    @jewishposts.to_json 
+  end
+
+  get '/api/blog/show/:id' do
+    @listing = Blog.find_by_id(params[:id])
+    content_type :json 
+    @listing.to_json
+  end
 end
