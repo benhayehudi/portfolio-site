4eab139c29079b6b4f8dd1ac03b15ebda51d0428
benhayehudi <bengreenberg@gmail.com>
2017-11-28 09:41:46 -0500

    added json controller routes for videos

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,24 @@
+class VideosController < ApplicationController
+  require 'sinatra'
+  require 'sinatra/cross_origin'
+
+  configure do
+    enable :cross_origin
+  end
+
+  # API Service
+  get '/api/videos/index.json' do
+    headers 'Access-Control-Allow-Origin' => 'http://www.bengreenberg.org'
+    @videos = Video.where(publish: [true])
+    content_type :json
+    @videos.to_json
+  end
+
+  get '/api/videos/show/:id' do
+    headers 'Access-Control-Allow-Origin' => 'http://www.bengreenberg.org'
+    @video = Video.find_by_id(params[:id])
+    content_type :json
+    @video.to_json
+  end
+  
+end
