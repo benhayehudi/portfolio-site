b2b434c9e22e90ce2c1c603049de488761182399
benhayehudi <bengreenberg@gmail.com>
2018-11-28 13:13:59 +0200

    updated allowed cors header

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,14 +8,14 @@ class VideosController < ApplicationController
 
   # API Service
   get '/api/videos/index.json' do
-    headers 'Access-Control-Allow-Origin' => 'http://www.bengreenberg.org'
+    headers 'Access-Control-Allow-Origin' => 'https://www.bengreenberg.org'
     @videos = Video.where(publish: [true])
     content_type :json
     @videos.to_json
   end
 
   get '/api/videos/show/:id' do
-    headers 'Access-Control-Allow-Origin' => 'http://www.bengreenberg.org'
+    headers 'Access-Control-Allow-Origin' => 'https://www.bengreenberg.org'
     @video = Video.find_by_id(params[:id])
     content_type :json
     @video.to_json
